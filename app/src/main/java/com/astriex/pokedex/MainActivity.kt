d7396a1b74b24cc92ed7aec2bfdfa2a23861a86d
Astriex <melishuskanovic@hotmail.com>
2021-11-23 15:55:51 +0100

    Initial commit

---
+++ app/src/main/java/com/astriex/pokedex/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.astriex.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.astriex.pokedex.ui.theme.PokedexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokedexTheme {
+                
+            }
+        }
+    }
+}
